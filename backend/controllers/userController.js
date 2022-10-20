9f7a31eb323ffa7dc75e5bf1e9c3af2cff580add
endritshala <72036616+endritshala@users.noreply.github.com>
2022-10-20 10:47:38 +0200

    Backend configuration
    Added signup function and hash passwords with bcrypt.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,21 @@
 const User = require("../models/userModel");
 
 // login user
-
 const loginUser = async (req, res) => {
   res.json({ mmsg: "Login user" });
 };
 
 // sign up user
-
 const signupUser = async (req, res) => {
-  res.json({ mssg: "Signup user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = { loginUser, signupUser };
