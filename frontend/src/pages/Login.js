66c7e2b1f3cfde079a0a65f86a8f535772b54ff7
endritshala <72036616+endritshala@users.noreply.github.com>
2022-10-23 13:31:19 +0200

    Authorized requests
    Added middleware for authorization when making requests.
    Without having the authorization token we can't access our workout information.
    When we are logged in we can see our workout details.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { login, error, isLoading } = useLogin();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    navigate("/");
 
     await login(email, password);
   };
